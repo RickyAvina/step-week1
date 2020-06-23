59e6b0649d21786eb4ad26242f1532972ea61958
Enrique Avina <rickyavina@icloud.com>
2020-06-23 00:38:16 +0000

    Added Interests section with a soccer slideshow. Currently images load in awkwardly, looking for a way to preload images

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,6 +12,32 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+$(document).ready(function() {
+    console.log("ready");
+    // locate gallery div
+    const pictureDiv = document.getElementById('gallery');
+
+    // cycle through photos
+    displayPhoto(pictureDiv, 0);
+    // const numImages = 5;
+    // var i = 0;
+    // setInterval(displayPhoto, 1000, pictureDiv, ++i)
+
+    // for (var i=0; i < numImages; i++) {
+    //     setInterval(displayPhoto, 1000, pictureDiv, i);
+    // }
+});
+
+function displayPhoto(div, index) {
+    if (index > 4) {
+        index = 0;
+    } 
+
+    console.log("index: " + index.toString());
+    div.innerHTML = "<img src='images/soccer/" + index.toString() + ".jpg'/>";
+
+    setTimeout(displayPhoto, 3000, div, index+1);
+}
 
 function getRandomImage() {
     // Pick random image name
@@ -26,3 +52,5 @@ function getRandomImage() {
     pictureContainer.innerHTML += "</br>" + "<p>These were all painted by \
                 <a href='https://www.mccallstudios.com/'>Robert McCall<a>, one of my favorite artists.</p>"
 }
+
+
