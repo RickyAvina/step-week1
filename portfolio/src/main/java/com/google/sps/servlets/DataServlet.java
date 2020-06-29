3ae63a03ac95663cc6840113a9fa92cea94b1ec9
Enrique Avina <rickyavina@icloud.com>
2020-06-29 22:29:41 +0000

    Add test response to DataServerlet /data path. Adds Cloud Client Libraries for Datastore API

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -26,7 +26,8 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    System.out.println("doGet() activated!");
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    response.getWriter().println("<h1>Hello Enrique! This is a test response.</h1>");
   }
 }
