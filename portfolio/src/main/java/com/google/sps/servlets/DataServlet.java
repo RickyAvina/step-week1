48e1fae36252e1f5a58db74189820c26d7ca5c9c
Enrique Avina <rickyavina@icloud.com>
2020-07-01 22:41:03 +0000

    Add form page that populates Comments data structure

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,6 +14,7 @@
 
 package com.google.sps.servlets;
 
+import com.google.sps.data.Comments;
 import java.io.IOException;
 import java.util.List;
 import java.util.Arrays;
@@ -21,10 +22,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDateTime;
 
 /** Servlet that returns writes greetings in many languages in JSON format */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
+
+  private Comments comments = new Comments();
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -49,6 +53,26 @@ public class DataServlet extends HttpServlet {
 
     System.out.println(sb.toString());
     response.getWriter().println(sb.toString());
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+      response.setContentType("application/json");
+
+      // get input from the form
+      String comment = request.getParameter("comment");
+      LocalDateTime now = LocalDateTime.now();
+      comments.addComment(now, comment);
+
+      // convert to JSON
+      String json = comments.json();
+    
+      response.setContentType("text/html;");
+      response.getWriter().println("<p>JSON\n: " + json + "</p>");
+    
+    //   System.out.println(json);
+
+      // redirect
   }
 
   /* Get hello world greetings in many different languages */
