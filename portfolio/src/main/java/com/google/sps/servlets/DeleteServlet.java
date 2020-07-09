1722d0b1fa4f3d273806be8f717550cadd24dbb2
Enrique Avina <rickyavina@icloud.com>
2020-07-09 19:23:32 +0000

    Fixed null check in DeleteServlet

--- portfolio/src/main/java/com/google/sps/servlets/DeleteServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteServlet.java
@@ -1,3 +1,18 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
 package com.google.sps.servlets;
 
 import com.google.sps.data.Comments;
@@ -35,11 +50,15 @@ public class DeleteServlet extends HttpServlet {
     PreparedQuery results = datastore.prepare(query);
 
     for (Entity entity : results.asIterable()) {
-      long comment_id = (long) entity.getProperty("comment_id");
-      if (comment_id == null) {
+
+      Object comment_id_obj = entity.getProperty("comment_id");
+      if (comment_id_obj == null) {
           System.err.println("comment_id of comment is NULL");
-          break;
+          response.sendRedirect("/home");
+          return;
       }
+
+      long comment_id = (long) comment_id_obj;
 
       if (Long.toString(comment_id).equals(id)) {
           datastore.delete(entity.getKey());
